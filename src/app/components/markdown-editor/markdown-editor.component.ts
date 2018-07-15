0496fcd60c54e9b775f96366d16c9aba9c96ec6e
Nikx Fabrizio <w3c.nadeem@gmail.com>
2018-07-16 02:05:09 +0530

    1

--- src/app/components/markdown-editor/markdown-editor.component.ts
+++ src/app/components/markdown-editor/markdown-editor.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormControl, FormBuilder } from '@angular/forms';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { MarkdownService } from '../markdown-editor/markdown.service';
-import { Subscription } from 'rxjs';
+import { listener } from '@angular/core/src/render3/instructions';
 
 
 @Component({
@@ -16,13 +16,26 @@ export class MarkdownEditorComponent implements OnInit {
       icon: ''
     }
   ];
-  private markdownSubscription: Subscription;
   markdownText: FormControl;
+  private _editorPanel: ElementRef;
+  @ViewChild('editorPanel') set editorPanel(panel) {
+    this._editorPanel = panel;
+  }
+  get editorPanel() {
+    return this._editorPanel.nativeElement;
+  }
   constructor(private markdownService: MarkdownService) {
-    this.markdownText = new FormControl();
   }
   ngOnInit() {
+    this.editorPanel.contentEditable = true;
+    this.markdownText = new FormControl();
     this.markdownService.markdownString = this.markdownText.valueChanges;
+
+    this.editorPanel.addEventListener('input', (event) => {
+      this.markdownText.setValue(event.target.innerText);
+      //      event.target.innerHTML = event.target.innerText;
+      //    event.target.focus();
+    });
   }
 
 }
