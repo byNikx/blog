ff4b3cae9204c2be812fde5980ba35a40da45b85
Nikx Fabrizio <w3c.nadeem@hotmail.com>
2018-07-26 17:19:57 +0530

    1

--- src/app/components/markdown-editor/markdown-editor.component.ts
+++ src/app/components/markdown-editor/markdown-editor.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener, AfterViewInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MarkdownService } from '../markdown-editor/markdown.service';
 import * as CkClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import * as Quill from 'quill';
 
 const CK_OPTIONS = {
   bodyClass: 'mat-typography',
@@ -25,26 +26,39 @@ const CK_OPTIONS = {
   }
 };
 
+const Q_TOOLBAR_OPTIONS = [
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  ['bold', 'italic', 'underline', 'strike'],
+];
+const QUILL_OPTIONS = {
+  placeholder: 'Compose an epic...',
+  theme: 'snow',
+  modules: {
+    toolbar: Q_TOOLBAR_OPTIONS
+  }
+};
+
 @Component({
   selector: 'nx-markdown-editor',
   templateUrl: './markdown-editor.component.html',
   styleUrls: ['./markdown-editor.component.scss']
 })
-export class MarkdownEditorComponent implements OnInit {
+export class MarkdownEditorComponent implements OnInit, AfterViewInit {
 
   private toolbarActions = [
     {
       icon: ''
     }
   ];
+  editorInstance: any;
   markdownText: FormControl;
-  // private _editorPanel: ElementRef;
-  // @ViewChild('editorPanel') set editorPanel(panel) {
-  //   this._editorPanel = panel;
-  // }
-  // get editorPanel() {
-  //   return this._editorPanel.nativeElement;
-  // }
+  private _editorPanel: ElementRef;
+  @ViewChild('editorPanel') set editorPanel(panel) {
+    this._editorPanel = panel;
+  }
+  get editorPanel() {
+    return this._editorPanel.nativeElement;
+  }
   constructor(private markdownService: MarkdownService) {
   }
   ngOnInit() {
@@ -56,6 +70,11 @@ export class MarkdownEditorComponent implements OnInit {
     //   .catch(error => {
     //     console.error(error);
     //   });
+
+  }
+
+  ngAfterViewInit() {
+    this.editorInstance = new Quill(this.editorPanel, QUILL_OPTIONS);
 
   }
 
