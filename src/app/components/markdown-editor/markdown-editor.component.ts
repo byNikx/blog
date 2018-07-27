be10a9683aa31ffd4d517a8e448a5a0ab2cf0c03
Nikx Fabrizio <w3c.nadeem@hotmail.com>
2018-07-27 17:44:15 +0530

    1

--- src/app/components/markdown-editor/markdown-editor.component.ts
+++ src/app/components/markdown-editor/markdown-editor.component.ts
@@ -1,30 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef, HostListener, AfterViewInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MarkdownService } from '../markdown-editor/markdown.service';
-import * as CkClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import * as Quill from 'quill';
-
-const CK_OPTIONS = {
-  bodyClass: 'mat-typography',
-  heading: {
-    options: [
-      // tslint:disable-next-line:max-line-length
-      { model: 'paragraph', view: { name: 'p', classes: 'mat-body-1' }, title: 'Paragraph', class: 'ck-heading_paragraph', priority: 'high' },
-      { model: 'display1', view: { name: 'h1', classes: 'mat-display-1' }, title: 'Display 1', class: 'ck-heading_heading1' },
-      { model: 'display2', view: { name: 'h1', classes: 'mat-display-2' }, title: 'Display 2', class: 'ck-heading_heading1' },
-      { model: 'display3', view: { name: 'h1', classes: 'mat-display-3' }, title: 'Display 3', class: 'ck-heading_heading1' },
-      { model: 'display4', view: { name: 'h1', classes: 'mat-display-4' }, title: 'Display 4', class: 'ck-heading_heading1' },
-      { model: 'heading1', view: { name: 'h1', classes: 'mat-headline' }, title: 'Heading 1', class: 'ck-heading_heading1' },
-      { model: 'heading2', view: { name: 'h2', classes: 'mat-title' }, title: 'Heading 2', class: 'ck-heading_heading2' },
-      { model: 'heading3', view: { name: 'h3', classes: 'mat-subheading-2' }, title: 'Heading 3', class: 'ck-heading_heading3' },
-      { model: 'heading4', view: { name: 'h4', classes: 'mat-subheading-1' }, title: 'Heading 4', class: 'ck-heading_heading4' },
-      { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
-      { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' },
-      { model: 'body1', view: { name: 'p', classes: 'mat-body-2' }, title: 'Body 2', class: 'mat-body-2' },
-      { model: 'body2', view: { name: 'p', classes: 'mat-body-1' }, title: 'Body 1', class: 'mat-body-1' }
-    ]
-  }
-};
 
 const Q_TOOLBAR_OPTIONS = [
   [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
@@ -53,28 +30,37 @@ export class MarkdownEditorComponent implements OnInit, AfterViewInit {
   editorInstance: any;
   markdownText: FormControl;
   private _editorPanel: ElementRef;
+  private _toolbar: ElementRef;
+
   @ViewChild('editorPanel') set editorPanel(panel) {
     this._editorPanel = panel;
   }
   get editorPanel() {
     return this._editorPanel.nativeElement;
   }
+
+  @ViewChild('toolbar') set toolbar(toolbar) {
+    this._toolbar = toolbar;
+  }
+  get toolbar() {
+    return this._toolbar.nativeElement;
+  }
+
+
   constructor(private markdownService: MarkdownService) {
   }
   ngOnInit() {
-    // CkClassicEditor
-    //   .create(this.editorPanel, CK_OPTIONS)
-    //   .then(editor => {
-    //     console.log(editor);
-    //   })
-    //   .catch(error => {
-    //     console.error(error);
-    //   });
-
   }
 
   ngAfterViewInit() {
-    this.editorInstance = new Quill(this.editorPanel, QUILL_OPTIONS);
+    this.editorInstance = new Quill(this.editorPanel, {
+      modules: {
+        toolbar: {
+          container: this.toolbar
+        }
+      },
+      theme: 'snow'
+    });
 
   }
 
