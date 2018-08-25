e0e78f3fc5b8cb2a3c785354846346348d3b0c7a
Nikx Fabrizio <w3c.nadeem@gmail.com>
2018-08-26 02:36:21 +0530

    1

--- src/app/components/container/container.component.ts
+++ src/app/components/container/container.component.ts
@@ -1,17 +1,18 @@
-import { Component, ElementRef, OnDestroy } from '@angular/core';
+import { Component, ElementRef, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { LayoutService } from '../../services/layout/layout.service';
 import { Subscription } from 'rxjs';
 
 
 @Component({
-  selector: 'nx-container',
-  host: {
-  	'[class]': 'activeClass'
-  },
-  template: `<ng-content></ng-content>`,
-  styleUrls: ['./container.component.css']
+	selector: 'nx-container',
+	host: {
+		'[class]': 'activeClass'
+	},
+	template: `<ng-content></ng-content>`,
+	styleUrls: ['./container.component.css'],
+	encapsulation: ViewEncapsulation.None
 })
-export class ContainerComponent implements OnDestroy{
+export class ContainerComponent implements OnDestroy {
 
 	_element: any;
 	activeClass: string;
@@ -24,14 +25,14 @@ export class ContainerComponent implements OnDestroy{
 		});
 	}
 
-	updateContainer(element, breakpoint): void{
-		if(!element)
+	updateContainer(element, breakpoint): void {
+		if (!element)
 			throw ReferenceError('No container defined.');
 
 		this.activeClass = breakpoint;
 	}
 
-	ngOnDestroy(){
+	ngOnDestroy() {
 		this.watcher.unsubscribe();
 	}
 
